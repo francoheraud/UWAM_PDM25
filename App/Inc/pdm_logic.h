b2b73412fc0d7ca3f575b416b51ee27713130ffd
Franco Heraud <francoheraud@Outlook.com>
2025-11-25 08:34:31 +0800

    WIP: adding adc conv

--- App/Inc/pdm_logic.h
+++ App/Inc/pdm_logic.h
@@ -8,12 +8,12 @@
 #ifndef INC_PDM_LOGIC_H_
 #define INC_PDM_LOGIC_H_
 
-#include "main.h"
-#include <stdint.h>
-#include <stdbool.h>
-#include <string.h>
-#include "lin_driver.h"
 #include "can_driver.h"
+#include "lin_driver.h"
+#include "main.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
 
 extern TIM_HandleTypeDef htim3;
 extern UART_HandleTypeDef huart4;
@@ -21,46 +21,32 @@ extern CAN_HandleTypeDef hcan;
 extern ADC_HandleTypeDef hadc2;
 extern ADC_HandleTypeDef hadc3;
 
-#define NUM_CHANNELS		7
-#define NUM_FUSES			7		// one fuse per switch circuit
-#define PDM_PWM_RES			255
-#define NUM_CURRENT_INPUTS	7
+#define NUM_CHANNELS 7
+#define NUM_FUSES 7 // one fuse per switch circuit
+#define PDM_PWM_RES 255
+#define NUM_CURRENT_INPUTS 7
+#define ADC_RES 4095 // 12-bit
 
-GPIO_TypeDef *fuse_input_ports[NUM_FUSES] = {
-		GPIOC,
-		GPIOC,
-		GPIOA,
-		GPIOB,
-		GPIOA,
-		GPIOC,
-		GPIOB
-};
+GPIO_TypeDef *fuse_input_ports[NUM_FUSES] = {GPIOC, GPIOC, GPIOA, GPIOB,
+                                             GPIOA, GPIOC, GPIOB};
 
-const uint16_t fuse_inputs[NUM_FUSES] = {
-		1,
-		3,
-		3,
-		7,
-		11,
-		5,
-		10
-};
+const uint16_t fuse_inputs[NUM_FUSES] = {1, 3, 3, 7, 11, 5, 10};
 
 typedef struct {
-	uint32_t code[NUM_CURRENT_INPUTS];
-	float volts[NUM_CURRENT_INPUTS];
-	float amps[NUM_CURRENT_INPUTS];
+  uint32_t code[NUM_CURRENT_INPUTS];
+  float volts[NUM_CURRENT_INPUTS];
+  float amps[NUM_CURRENT_INPUTS];
 } ADC_t;
 
 typedef struct {
-	uint8_t fuse;
-	bool mcu;
+  uint8_t fuse;
+  bool mcu;
 } Status_t;
 
 typedef struct {
-	CAN_Driver_t can;
-	ADC_t adc;
-	Status_t state;
+  CAN_Driver_t can;
+  ADC_t adc;
+  Status_t state;
 } PDM_t;
 
 #endif /* INC_PDM_LOGIC_H_ */
